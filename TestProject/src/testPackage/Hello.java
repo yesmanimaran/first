e64c1f91b73862e9f7e235e20606788a5cefae48
manimaran <mani@easysolutions.in>
2016-06-03 16:44:53 +0530

    Merged with remote system

--- TestProject/src/testPackage/Hello.java
+++ TestProject/src/testPackage/Hello.java
@@ -8,6 +8,7 @@ public class Hello
 		System.out.println("Hello World!");
 		System.out.println("Welcome to Git World");
 		System.out.println("Local Changes");
+		System.out.println("You can do anything with GIT !!! Enjoy!!!");
 	}
 
 }
